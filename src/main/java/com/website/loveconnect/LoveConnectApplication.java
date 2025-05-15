c21a041ecb938d3ab50f6c9f291ef0a94d85541b
WhyNotYud <mtd.contactwithme@gmail.com>
2025-05-15 23:46:56 +0700

    chore: init project

---
+++ src/main/java/com/website/loveconnect/LoveConnectApplication.java
@@ -0,0 +1,13 @@
+package com.website.loveconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveConnectApplication.class, args);
+    }
+
+}
